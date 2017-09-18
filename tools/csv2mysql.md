e4b871da6ce58f1832bd122b88d8854cc34e6028
Chao Gou <gou_chao@icekredit.com>
2017-09-18 11:39:25 +0800

    add csv2mysql tool

---
+++ tools/csv2mysql.md
@@ -0,0 +1,15 @@
+# csv2mysql for PHP#
+
+Creates an importable MySQL file from a CSV file.
+
+## Usage ##
+
+Put the CSV file in the bin folder.  From command line:
+
+```php
+php csv2mysql.php [input.csv] [mysql_table_name.sql] [mysql_database_name]
+```
+
+The script doesn't actually do anything to your database, but the importable file will need to know the destination database and table.
+
+this script is forked from `https://github.com/swt83/php-csv2mysql`, I did some changes to fit my task.
---
+++ tools/csv2mysql.php
@@ -0,0 +1,115 @@
+<?php
+
+/**
+ * A standalone class to convert CSV files into importable MySQL files.
+ *
+ * @package    csv2mysql
+ * @author     Scott Travis <scott.w.travis@gmail.com>
+ * @link       http://github.com/swt83/php-csv2mysql
+ * @license    MIT License
+ */
+
+
+/*
+|--------------------------------------------------------------------------
+| Set variables for conversion.
+|--------------------------------------------------------------------------
+*/
+
+$import_file = 'bin/'.$argv[1];
+$export_file = 'bin/'.$argv[2];
+$database = $argv[3];
+$table = basename($export_file, '.sql');
+
+
+/*
+|--------------------------------------------------------------------------
+| Run initial scan of CSV file.
+|--------------------------------------------------------------------------
+*/
+
+if (($input = @fopen($import_file, 'r')) != false)
+{
+    $row = 1;
+    while (($fields = fgetcsv($input, 1000, ',')) != false)
+    {
+        if ($row == 1)
+        {
+            foreach ($fields as $field)
+            {
+                $headers[] = strtolower(str_ireplace(' ', '_', $field));
+            }
+        }
+        else
+        {
+            foreach ($fields as $key=>$value)
+            {
+                if (!isset($max_field_lengths[$key]))
+                {
+                    $max_field_lengths[$key] = 0;
+                }
+
+                if (strlen($value) > $max_field_lengths[$key])
+                {
+                    $max_field_lengths[$key] = strlen($value);
+                }
+                $field++;
+            }
+        }
+        $row++;
+    }
+    fclose($input);
+}
+else
+{
+    echo 'Unable to open file "'.$import_file.'".'."\n";
+}
+
+
+/*
+|--------------------------------------------------------------------------
+| Build new importable SQL file.
+|--------------------------------------------------------------------------
+*/
+
+$output = fopen($export_file, 'w');
+fwrite($output, "CREATE DATABASE IF NOT EXISTS {$database};\n");
+fwrite($output, 'CREATE TABLE `'.$database.'`.`'.$table.'` ('."\n");
+fwrite($output, '`id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,'."\n");
+foreach ($headers as $key=>$header)
+{
+    fwrite($output, '`'.$header.'` VARCHAR('.$max_field_lengths[$key].') NOT NULL,'."\n");
+}
+fwrite($output, 'PRIMARY KEY (`id`)'."\n".') DEFAULT CHARACTER SET \'utf8mb4\';'."\n"."\n");
+if (($input = @fopen($import_file, 'r')) != false)
+{
+    $row = 1;
+    while (($fields = fgetcsv($input, 1000, ',')) != false)
+    {
+        if (sizeof($fields) != sizeof($headers))
+        {
+            echo 'INCORRECT NUMBER OF FIELDS  (search your file for \'\"\' string):';
+            echo print_r($fields, true);
+            die();
+        }
+
+        if ($row != 1)
+        {
+            $sql = 'INSERT INTO `'.$database.'`.`'.$table.'` VALUES(null, ';
+            foreach ($fields as $field)
+            {
+                $sql .= '\''.mysql_real_escape_string($field).'\', ';
+            }
+            $sql = rtrim($sql, ', ');
+            $sql .= ');';
+            fwrite($output, $sql."\n");
+        }
+        $row++;
+    }
+    fclose($input);
+}
+else
+{
+    echo 'Unable to open file "'.$import_file.'".'."\n";
+}
+fclose($output);

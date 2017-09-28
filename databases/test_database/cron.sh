18a8038f683b964f3be4c9f2d9147e655ccc8c9a
Chao Gou <gou_chao@icekredit.com>
2017-09-28 16:51:49 +0800

    Add this script to cron

--- databases/test_database/cron.sh
+++ databases/test_database/cron.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+#Add this script to cron. For example, every morning 03:00 run backup: `0 3 * * * /bin/bash /path/to/Increment_Backup_To_Hive/databases/test_database/cron.sh`
+
 #cron执行时注意更新环境变量，使hive和php命令可用
 source /etc/profile
 

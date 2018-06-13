c5a6618cbaf75f1c14fa8ad87fa5f31c56b9325c
Chao Gou <gou_chao@icekredit.com>
2018-06-13 16:16:06 +0800

    dev

--- tools/centos7-php70-install.sh
+++ tools/centos7-php70-install.sh
@@ -3,3 +3,4 @@
 yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 yum -y install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
 yum --enablerepo=remi -y install php70 php70-php-pdo php70-php-mysqlnd php70-php-mbstring php70-php-xml php70-php-mcrypt
+ln -s /bin/php70 /bin/php
